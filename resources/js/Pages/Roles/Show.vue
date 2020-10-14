abb499d8ed4a4793e019bc0e777cf5d659b307c8
renzosiza7 <rsiza@unsa.edu.pe>
2020-10-14 10:04:03 -0500

    Modificación interfaz login

--- resources/js/Pages/Roles/Show.vue
+++ resources/js/Pages/Roles/Show.vue
@@ -1,37 +1,41 @@
 <template>
-  <app-layout>
-    <template #icon_title>
-      <i class="fa fa-box fa-fw"></i>
-    </template>
-    <template #title>Roles</template>
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Mostrar Rol</h3>
-      </div>
-      <div class="card-body">
-        <b-form @submit.prevent="registrar">
-          <b-form-group id="input-group-2" label="Nombre:" label-for="input-2">
-            <b-form-input
-              id="input-2"
-              v-model="rol.nombre"
-              placeholder="Nombre de rol"
-              readonly
-            ></b-form-input>
-          </b-form-group>
-        </b-form>
-      </div>
-    </div>
-  </app-layout>
+    <app-layout>
+        <template #icon_title>
+            <i class="fa fa-box fa-fw"></i>
+        </template>
+        <template #title>Roles</template>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Mostrar Rol</h3>
+            </div>
+            <div class="card-body">
+                <b-form>
+                    <b-form-group
+                        id="input-group-2"
+                        label="Nombre:"
+                        label-for="input-2"
+                    >
+                        <b-form-input
+                            id="input-2"
+                            v-model="rol.nombre"
+                            placeholder="Nombre de rol"
+                            readonly
+                        ></b-form-input>
+                    </b-form-group>
+                </b-form>
+            </div>
+        </div>
+    </app-layout>
 </template>
 
 <script>
 import AppLayout from "./../../Layouts/AppLayout";
 
 export default {
-  name: "roles.show",
-  props: ["rol"],
-  components: {
-    AppLayout,
-  },
+    name: "roles.show",
+    props: ["rol"],
+    components: {
+        AppLayout
+    }
 };
 </script>
