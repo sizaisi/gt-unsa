f888e0e9cd6d2a5d7fba8c700f305ccd6a2394f9
Renzo Siza Tejada <rsiza@unsa.edu.pe>
2021-01-05 23:34:04 -0500

    refresh main branch

---
+++ storage/framework/views/e665efb3c0a122f6851c02e2c97475a039ea26ac.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+
+        <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
+    </head>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            <?php echo e($slot); ?>
+
+        </div>
+    </body>
+</html>
+<?php /**PATH D:\MIS PROYECTOS\RDC\autopartes\resources\views/layouts/guest.blade.php ENDPATH**/ ?>
