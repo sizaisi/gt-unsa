d6547f126ceb5cc39aaa0be5873c4be4c4c5e0d6
Renzo Siza Tejada <rsiza@unsa.edu.pe>
2021-01-14 10:26:48 -0500

    modificación de grado modalidad en colaciones

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Configuración inicial
 
 -   php artisan db:seed --class=ConfigurationSeeder
+
+## Para correr solo una migración
+php artisan migrate:refresh --path=/database/migrations/2021_01_08_205955_create_gt_colaciones_table.php
+
--- app/Http/Controllers/ExpedienteController.php
+++ app/Http/Controllers/ExpedienteController.php
@@ -16,14 +16,14 @@ class ExpedienteController extends Controller
         ->leftjoin('gt_graduando', 'gt_graduando.id', '=', 'gt_graduando_expediente.idgraduando')
         ->leftjoin('actescu', 'actescu.nues', '=', 'gt_expediente.nues')
         ->leftjoin('gt_procedimientos', 'gt_procedimientos.id', '=', 'gt_expediente.idprocedimiento')
-        ->leftjoin('gt_grado_modalidades', 'gt_grado_modalidades.id', '=', 'gt_procedimientos.idgradomodalidad')
+        ->leftjoin('gt_tramites', 'gt_tramites.id', '=', 'gt_procedimientos.idtramite')
         ->leftjoin('gt_roles', 'gt_roles.id', '=', 'gt_procedimientos.idrol')
         ->leftjoin('gt_colaciones', 'gt_colaciones.id', '=', 'gt_expediente.idcolacion')
         ->select('gt_expediente.*', 'gt_graduando.id as idgraduando', 'gt_graduando.cui as cui', 
         'gt_graduando.name as nombre', 'gt_graduando.email as email', 'gt_graduando.telefono_movil as celular', 'gt_graduando.direccion as direccion',
-        'actescu.nesc as escuela', 'gt_procedimientos.nombre as procedimiento', 'gt_grado_modalidades.componente as modalidadobtencion',
+        'actescu.nesc as escuela', 'gt_procedimientos.nombre as procedimiento', 'gt_tramites.nombre as modalidadobtencion',
         'gt_roles.nombre as rol', 'gt_colaciones.fecha as fecha_colacion')
-        ->get();     
+        ->get();          
 
         return Inertia::render('Expedientes/Index', compact('expedientes'));
     }    
